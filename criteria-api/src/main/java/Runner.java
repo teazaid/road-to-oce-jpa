d9a8e5909a73d03349b42d568c4b3293e51aaee7
alexander_zaidel <alexander.zaidel@kredito.de>
2016-02-17 10:55:42 +0200

    added criteria-api example

--- criteria-api/src/main/java/Runner.java
+++ criteria-api/src/main/java/Runner.java
@@ -1,6 +1,12 @@
+import guru.zaidel.model.Employee;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  * Created by alexanderz on 16.02.16.
@@ -10,7 +16,29 @@ public class Runner {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("criteria-api-unit");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        Employee employee = new Employee();
+        employee.setName("Tester");
 
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.persist(employee);
+
+        transaction.commit();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Employee> query = criteriaBuilder.createQuery(Employee.class);
+        Root<Employee> emp = query.from(Employee.class);
+
+        query.select(emp).where(criteriaBuilder.equal(emp.get("name"), "Tester"));
+
+//        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+//        cb.createQuery();
+//        cb.createQuery(Class.class);
+//        cb.createTupleQuery()
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        cb.createQuery().
 
         entityManager.close();
         entityManagerFactory.close();
--- criteria-api/src/main/java/guru/zaidel/model/Employee.java
+++ criteria-api/src/main/java/guru/zaidel/model/Employee.java
@@ -1,7 +1,34 @@
 package guru.zaidel.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by alexanderz on 16.02.16.
  */
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
